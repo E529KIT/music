88a2af63dd475f373d0e501552a3899167e97265
TatsuyaOba <tOba1357@users.noreply.github.com>
2016-11-28 22:42:30 +0900

    テストモデルにTensor Board用のコード付け足し (#2)
    LGTM

--- test/mnist.py
+++ test/mnist.py
@@ -20,38 +20,77 @@
 See extensive documentation at
 http://tensorflow.org/tutorials/mnist/beginners/index.md
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import argparse
-import sys
-
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-# Create the model
-x = tf.placeholder(tf.float32, [None, 784])
-W1 = tf.Variable(tf.truncated_normal([784, 784], stddev=0.1))
-b1 = tf.Variable(tf.truncated_normal([784], stddev=0.1))
-logits = tf.matmul(x, W1) + b1
-W2 = tf.Variable(tf.truncated_normal([784, 10], stddev=0.1))
-b2 = tf.Variable(tf.truncated_normal([10], stddev=0.1))
-logits = tf.matmul(logits, W2) + b2
 
+# Create the model
+def variable_summaries(var, name):
+    """varの値の変動をtensorboardで確認できるようにする"""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.scalar_summary('mean/' + name, mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.scalar_summary('stddev/' + name, stddev)
+        tf.scalar_summary('max/' + name, tf.reduce_max(var))
+        tf.scalar_summary('min/' + name, tf.reduce_min(var))
+        tf.histogram_summary(name, var)
+
+
+def create_layer(input_tensor, input_dim, output_dim, layer_name, activation_function=tf.nn.relu):
+    """
+    input_tensorに新しいoutput_dim数のレイヤー付け加える
+    :param input_tensor:
+    :param input_dim:
+    :param output_dim:
+    :param layer_name:
+    :param activation_function:
+    :return: 付け加えたあとのtensor
+    """
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            weights = tf.Variable(tf.truncated_normal([input_dim, output_dim], stddev=0.1))
+            variable_summaries(weights, layer_name + '/weights')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.truncated_normal([output_dim], stddev=0.1))
+            variable_summaries(biases, layer_name + '/biases')
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+        activations = activation_function(preactivate)
+        tf.histogram_summary(layer_name + '/activations', activations)
+        return activations
+
+
+x = tf.placeholder(tf.float32, [None, 784])
+logits = create_layer(x, 784, 784, "layer1")
+logits = create_layer(logits, 784, 10, "layer2", activation_function=tf.nn.softmax)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_))
+tf.scalar_summary('loss', cross_entropy)
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 sess.run(tf.initialize_all_variables())
+
+# For Tensor Board
+merged = tf.merge_all_summaries()
+train_writer = tf.train.SummaryWriter('./log', sess.graph)
+
 # Train
-for _ in range(1000):
+for step in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    feed_dict = {x: batch_xs, y_: batch_ys}
+    if step % 10 == 0:
+        # record weight and biases
+        _, summaries = sess.run([train_step, merged], feed_dict)
+        train_writer.add_summary(summaries, step)
+    else:
+        sess.run(train_step, feed_dict)
 
 # Test trained model
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
