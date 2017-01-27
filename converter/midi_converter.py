76463942b35be5df72863a3a3a48c726196533b6
Tatsuya Oba <toba17893@gmail.com>
2017-01-27 20:34:39 +0900

    github/issue#70 create converter from midi to train data

--- converter/midi_converter.py
+++ converter/midi_converter.py
@@ -9,14 +9,24 @@ def load_file(filename):
 
 # todo: 途中でテンポの変わる曲にも対応できると良い
 # todo: 音の大きさの変化も学習できるといいかも
-def convert_PrettyMIDI_to_train_data(midi):
+def convert_PrettyMIDI_to_train_data(midi, pitch_sequence=True, bar_size=32):
+    '''
+    midiから訓練データを作成する。
+    ここで生成される訓練データは、一生節あたりのsequence_lengthがbar_sizeで、one_hotみたいな形で[音のなっている時間][音の高さ] = 1となっている。
+    :param midi: pretty_midi
+    :param pitch_sequence: True or False.
+    Trueのとき、一つの音の開始から終了まで１で埋める。
+    Falseのとき、音のなり始めのところのみを1とする。
+    :param bar_size: 一生節あたりの訓練データのsequenceサイズ
+    :return: [len(instruments), train_data_sequence, 128]
+    '''
     if not isinstance(midi, pretty_midi.PrettyMIDI):
         raise Exception('arg != PrettyMIDI')
     end_time = midi.get_end_time()
     tempo_change_times, tempi = midi.get_tempo_changes()
     if len(tempo_change_times) > 1:
         raise Exception("change tempo in music")
-    one_data_sec = 60 / tempi[0] / 8
+    one_data_sec = 60 / tempi[0] / bar_size / 4
     train_data_size = int(round(end_time / one_data_sec))
 
     # 同じ楽器で演奏されているものはまとめる。
@@ -36,8 +46,11 @@ def convert_PrettyMIDI_to_train_data(midi):
         for note in instrument.notes:
             start_time = int(round(note.start / one_data_sec))
             end_time = int(round(note.end / one_data_sec))
-            for time in range(start_time, end_time):
-                train_data[train_data_index][time][note.pitch] = 1
+            if pitch_sequence:
+                for time in range(start_time, end_time):
+                    train_data[train_data_index][time][note.pitch] = 1
+            else:
+                train_data[train_data_index][start_time][note.pitch] = 1
     return train_data
 
 
--- launcher/multi_midi_v2/train_launcher.py
+++ launcher/multi_midi_v2/train_launcher.py
@@ -5,8 +5,9 @@ import tensorflow as tf
 
 sys.path.append("../../")
 
-from launcher.common.train import train_has_batch
+from converter.train_data_converter import create_midi_train_data_set_v2
 from model.LSTM_music import LSTM
+from launcher.common.train import train_has_batch
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string("logdir", "log/1", "log dir")
@@ -29,14 +30,13 @@ def main(argv):
     config = Config
     midi_dir = "../midi/multi_train_midi"
     midi_files = glob.glob(midi_dir + "/*.midi") + glob.glob(midi_dir + "/*.mid")
-    # TODO: create dataset from midis
-    train_data = None
+    train_data = create_midi_train_data_set_v2(midi_files, config.sequence_length, config.pitch_size, config.bar_size)
     with tf.variable_scope("model"):
         model = LSTM(config)
 
     with tf.Session() as session:
         saver = tf.train.Saver(tf.trainable_variables())
-        train_has_batch(session, model, train_data, 0, config.bar_size, saver, FLAGS.logdir, FLAGS.load_dir)
+        train_has_batch(session, model, train_data, 0, config.batch_size, saver, FLAGS.logdir, FLAGS.load_dir)
 
 
 if __name__ == '__main__':
