5e0f3cbe1013c86cdadfb6ae1a8148d0c128ba65
Tatsuya Oba <toba17893@gmail.com>
2017-02-03 22:18:44 +0900

    add train log and abstract generater

--- launcher/common/generate.py
+++ launcher/common/generate.py
@@ -1,15 +1,23 @@
-
-def generate(session, model, start_inputs, size):
+def generate(session, model, start_inputs, size, norm_func=None):
     state = session.run(model.initial_state)
     fetches = [model.logits, model.last_state]
     result = [start_inputs[0][0]]
+    logits = []
+    start_input_size = len(start_inputs)
+
     for input_ in start_inputs:
         feed_dict = {model.inputs: [input_], model.initial_state: state}
         logits, state = session.run(fetches, feed_dict)
-        result.append(logits[0])
+        logits = logits[0][0]
+        if norm_func is not None:
+            logits = norm_func(logits)
+        result.append(logits)
 
-    for i in range(size - len(start_inputs)):
-        feed_dict = {model.inputs: [[result[-1]]], model.initial_state: state}
+    for i in range(size - start_input_size):
+        feed_dict = {model.inputs: [[logits]], model.initial_state: state}
         logits, state = session.run(fetches, feed_dict)
-        result.append(logits[0])
+        logits = logits[0][0]
+        if norm_func is not None:
+            logits = norm_func(logits)
+        result.append(logits)
     return result
