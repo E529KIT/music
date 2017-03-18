a76cc2b6e28e8d71d8f26709646cf0ea29b96975
TatsuyaOba <tOba1357@users.noreply.github.com>
2017-03-18 23:08:43 +0900

    read train data set from TFRecord (#79)

---
+++ launcher/common/sequence_example.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+
+def create_sequence_example(inputs, labels):
+    input_features = [
+        tf.train.Feature(float_list=tf.train.FloatList(value=input_))
+        for input_ in inputs]
+    label_features = [
+        tf.train.Feature(float_list=tf.train.FloatList(value=label))
+        for label in labels]
+    feature_list = {
+        'inputs': tf.train.FeatureList(feature=input_features),
+        'labels': tf.train.FeatureList(feature=label_features)
+    }
+    feature_lists = tf.train.FeatureLists(feature_list=feature_list)
+    return tf.train.SequenceExample(feature_lists=feature_lists)
+
+
+def get_padded_batch(file_list, batch_size, input_size, label_size,
+                     sequence_length, num_enqueuing_threads=4):
+    file_queue = tf.train.string_input_producer(file_list)
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(file_queue)
+
+    sequence_features = {
+        'inputs': tf.FixedLenSequenceFeature(shape=[input_size],
+                                             dtype=tf.float32),
+        'labels': tf.FixedLenSequenceFeature(shape=[label_size],
+                                             dtype=tf.float32)
+    }
+
+    context, sequence = tf.parse_single_sequence_example(
+        serialized_example, sequence_features=sequence_features)
+
+    length = tf.shape(sequence['inputs'])[0]
+
+    queue = tf.PaddingFIFOQueue(
+        capacity=1000,
+        dtypes=[tf.float32, tf.float32, tf.int32],
+        shapes=[(sequence_length, input_size), (sequence_length, label_size), ()])
+
+    enqueue_ops = [queue.enqueue([sequence['inputs'],
+                                  sequence['labels'],
+                                  length])] * num_enqueuing_threads
+    tf.train.add_queue_runner(tf.train.QueueRunner(queue, enqueue_ops))
+    return queue.dequeue_many(batch_size)
--- launcher/multi_midi_v2/train_launcher.py
+++ launcher/multi_midi_v2/train_launcher.py
@@ -5,9 +5,8 @@ import tensorflow as tf
 
 sys.path.append("../../")
 
-from converter.train_data_converter import create_midi_train_data_set_v2
 from model.LSTMContainedMusicTheory import Model
-from launcher.common.train import train_has_batch
+from launcher.common.sequence_example import get_padded_batch
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string("logdir", "log/1", "log dir")
@@ -27,15 +26,17 @@ class Config:
 
 
 def main(argv):
-    config = Config
-    midi_dir = "../../midi/multi_train_midi"
-    midi_files = glob.glob(midi_dir + "/*.midi") + glob.glob(midi_dir + "/*.mid")
-    train_data = create_midi_train_data_set_v2(midi_files, config.sequence_length, config.pitch_size, config.bar_size)
-    model = Model(config)
+    with tf.Graph().as_default() as graph:
+        config = Config
+        file_list = glob.glob("./train_data/*")
+        inputs, labels, _ = get_padded_batch(file_list, config.batch_size, 160, 160, config.sequence_length)
+        model = Model(config, inputs=inputs, labels=labels)
 
-    with tf.Session() as session:
-        saver = tf.train.Saver(tf.trainable_variables())
-        train_has_batch(session, model, train_data, 0, config.batch_size, saver, FLAGS.logdir, FLAGS.load_dir)
+    sv = tf.train.Supervisor(graph=graph, logdir=FLAGS.logdir, save_model_secs=30,
+                             global_step=model.global_step)
+    with sv.managed_session() as session:
+        while (True):
+            session.run(model.train_optimizer)
 
 
 if __name__ == '__main__':
