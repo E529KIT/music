bug fix : gradient is Nan for 0 * log(0) at cross entropy

launcher/multi_midi_v2/train_launcher.py
@@ -31,10 +31,10 @@ def main(argv):
         config = Config
         file_list = glob.glob("./train_data/*")
         inputs, labels, _ = get_padded_batch(file_list, config.batch_size, 160, 160, config.sequence_length)
-        model = Model(config, inputs=inputs, labels=labels)
+        model = Model(config, inputs=inputs, labels=labels, activate_function=tf.nn.sigmoid)
 
     sv = tf.train.Supervisor(graph=graph, logdir=FLAGS.logdir, save_model_secs=30,
-                             global_step=model.global_step)
+                             global_step=model.global_step, save_summaries_secs=60)
     with sv.managed_session() as session:
         global_step = session.run(model.global_step)
         while True:

model/LSTMContainedMusicTheory.py
@@ -84,7 +84,8 @@ class Model:
             bar_logits = tf.slice(logits, [0, 0, pitch_size], [-1, -1, bar_size])
             self._bar_logits = bar_logits = tf.nn.softmax(bar_logits)
             bar_logits_flat = tf.reshape(bar_logits, [-1, bar_size])
-            self._bar_loss = bar_loss = tf.reduce_mean(-bar_labels_flat * tf.log(bar_logits_flat), name="bar_loss")
+            self._bar_loss = bar_loss = tf.reduce_mean(
+                -bar_labels_flat * tf.log(tf.clip_by_value(bar_logits_flat, 1e-10, 1.0)), name="bar_loss")
             tf.summary.scalar('bar_loss', bar_loss)
 
             pitch_labels = tf.slice(labels, [0, 0, 0], [-1, -1, pitch_size])